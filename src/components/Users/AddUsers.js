5e449d7f7396d5f7b303989fb1c51c6b809cb039
Harish1651999 <hk1651999@gmail.com>
2022-10-30 10:37:04 +0530

    add validation and resetting logic.

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddUsers.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
-const AddUsers = () => {
-  const addUserHandler = (e) => {
-    e.preventDefault();
+const AddUsers = (props) => {
+  const [enteredusername, setEnteredUsername] = useState("");
+  const [enteredage, setEnteredAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredusername.trim().length === 0 || enteredage.trim().length === 0) {
+      return;
+    }
+    if (+enteredage < 0) {
+      return;
+    }
+    console.log(enteredusername, enteredage);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">User Name</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredusername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number" />
-        <Button type="button">Add User</Button>
+        <input
+          id="age"
+          type="number"
+          value={enteredage}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
